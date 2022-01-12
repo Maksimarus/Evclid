4c0fe8ba6ea4c2175195fed902a73f0c4274e91e
Чучук Максим <cucukmaksim@MacBook-Air-Cucuk.local>
2022-01-12 18:28:23 +0300

    added accordion, swiper, burger

--- JS/index.js
+++ JS/index.js
@@ -3,3 +3,38 @@ window.addEventListener('DOMContentLoaded', function() {
 		document.querySelector('#menu').classList.toggle('is-active')	
 	})
 })
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	loop: true,
+	autoHeight: true,
+ 
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+ 
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+ });
+ $( function() {
+	var icons = {
+      header: null,
+      activeHeader: null,
+    };
+	$( "#accordion" ).accordion({
+		collapsible: true,
+		heightStyle: "content",
+		header: "> div > h3",
+		icons: icons,
+	})
+	$( "#accordion" ).accordion( "option", "icons", null )
+		.sortable({
+			axis: "y",
+			handle: "h3",
+			stop: function( event, ui ) {
+			ui.item.children( "h3" ).triggerHandler( "focusout" );
+			$( this ).accordion( "refresh" );
+		 }
+	  });
+ } );
